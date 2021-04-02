6e2e109efac8cd660c83a1c8e8f1233b48c69f54
ohimma <ohimma@tengtengdeMacBook-Pro.local>
2021-04-02 10:18:39 +0800

    delete chatroom and add pkg

--- hatch_chatroom/common/message/user.go
+++
@@ -1,8 +0,0 @@
-package message
-
-type User struct {
-	UserID     int    `json: "userid"`
-	UserPwd    string `json: "userpwd"`
-	UserName   string `json: "username"`
-	UserStatus int    `json: "userstatus"`    // 用户状态
-}
